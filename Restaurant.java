33aef11058dc8d89b564be181eedf11942b73213
Robert van den Heuvel <robert_vd_heuvel@hotmail.com>
2019-02-20 17:33:52 +0100

    alleen verkeerde bestelling nog fixen

--- Restaurant.java
+++ Restaurant.java
@@ -4,13 +4,14 @@ public class Restaurant{
 		Gast gast = new Gast();
 		gast.naam = "Piet";
 		gast.geld = 15;
-		Bestelling bestelling = new Bestelling();
-		bestelling.bestelling = "stampot";
-		bestelling.controleren();
-		gast.bestellen(bestelling);
-		//de ober geeft de bestelling door aan de chef
+		gast.bestelling1 = new Bestelling();
+		gast.bestelling1.bestelling = "stamot";
+		gast.bestellen(gast.bestelling1);
+		gast.bestelling1.controleren();
+		Ober ober = new Ober();
+		ober.doorgeven(gast.bestelling1);
 		Chef chef = new Chef();
-		chef.koken(bestelling);
+		chef.koken(gast.bestelling1);
 		gast.afrekenen();
 
 
@@ -20,10 +21,9 @@ public class Restaurant{
 class Gast{
 	String naam;
 	int geld;
-	//methode bestelling
+	static Bestelling bestelling1;
 	void bestellen(Bestelling bestelling1){
-		
-		System.out.println("bestelling is " + bestelling1.bestelling);
+		System.out.println(naam + " bestelt " + bestelling1.bestelling + ".");
 	}
 	void afrekenen(){
 		if (geld<10) {
@@ -46,14 +46,21 @@ class Bestelling{
 	}
 }
 class Ober{
-// neemt bestelling eten (has Bestelling)
-// methode geeft bestelling aan chef
+	void doorgeven(Bestelling bestelling1){
+		if (bestelling1.controleren = true) {
+			System.out.println("De ober geeft de bestelling door aan de chef.");
+		}else {
+			System.out.println("De bestelling wordt verkeerd doorgegeven.");
+		}
+	}
 }
 class Chef{
-	//ingredienten
+	String ingredient1 = "aardappel";
+	String ingredient2 = "boerenkool";
+	String ingredient3 = "rookworst";
 	void koken(Bestelling bestelling1){
 		if (bestelling1.controleren = true) {
-		System.out.println("De chef maakt stampot.");
+		System.out.println("De chef maakt stampot met " + ingredient1 + ", "+ ingredient2 + ", en " + ingredient3 + ".");
 		}else {
 			System.out.println("De chef maakt het verkeerde gerecht!");
 		}
